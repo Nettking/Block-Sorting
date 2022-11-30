262abc4b1feb1f42c2b4feb89a2dad5aa574eef5
Björn Andersson <55714816+bjornsan@users.noreply.github.com>
2022-11-30 08:23:06 +0100

    latest

---
+++ Camera.py
@@ -0,0 +1,51 @@
+import time
+import urllib.request
+
+class Camera:
+    def __init__(self, ip):
+        self.ip = ip        
+
+    def getRes(self):
+        res = urllib.request.urlopen('http://'+self.ip+'/CmdChannel?TRIG')
+        time.sleep(2)
+        res = urllib.request.urlopen('http://'+self.ip+'/CmdChannel?gRES')
+        time.sleep(2)
+        return res
+
+    def processRes(self):
+        res = self.getRes()
+        x_offset = float(25)
+        y_offset = float(-485)
+
+        #reads output from camera
+        coords = res.read().decode('utf-8')
+        #splits output
+        x1 = coords.split(",")
+        #whichObject = int(x1[1])
+        objectLocated = int(x1[2])
+
+        # check if no block
+        if len(x1) < 2:
+            # No block found
+            pass # her bør vi ha noen feilhåndtering
+        else:
+            # NO. Blocks > 0
+            if objectLocated == 1:
+                y = x1[4]
+                x = x1[3]
+                x = (float(x) + x_offset) /1000
+                y = (float(y) - y_offset) /1000
+                time.sleep(3)
+                return x, y
+            
+    def setPositionFromCameraInput(self):
+        x,y = self.processRes()
+        # Left side has reversed y-value 
+        positionPickUp = (x,-y,0.05,0.05,3.14,0)
+        return positionPickUp
+    
+    def checkForBlock(self):
+        '''Check for block'''
+        x, y = self.processRes()
+        return x, y
+
